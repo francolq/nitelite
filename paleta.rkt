3fbf79934f8c231f596d497319cb30f63e3cf2e7
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-11-06 19:01:02 -0300

    Paleta de colores.

---
+++ paleta.rkt
@@ -0,0 +1,9 @@
+; paleta de colores
+(define p0 (vector 1 (/ 233 255) (/ 199 255)))  ; FFE9C7
+(define p1 (vector (/ 254 255) 1 (/ 125 255)))  ; FEFF7D
+(define p2 (vector 1 (/ 84 255) (/ 58 255)))    ; FF543A
+(define p3 (vector 1 0 (/ 172 255)))            ; FF00AC
+(define p4 (vector (/ 129 255) (/ 7 255) (/ 90 255))) ; 81075A
+(define p5 (vector (/ 27 255) (/ 7 255) (/ 19 255)))  ; 1B0713
+
+(define paleta (list p0 p1 p2 p3 p4 p5))
