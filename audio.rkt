106d437a8898c4ae5eb0461c3186554366a976ec
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-12-14 20:38:43 -0300

    Entrada de audio.

---
+++ audio.rkt
@@ -0,0 +1,9 @@
+(start-audio "PulseAudio JACK Sink:front-left" 1024 44100)
+
+; test input
+;(hint-wire)
+;(gain 0.1)
+;(define (render)
+;    (colour (vector (gh 1) (gh 5) (gh 9)))
+;    (draw-cube))
+;(every-frame (render))
