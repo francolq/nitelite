f5c941069d6adc439364774dc0e57ba2c68daa99
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-12-15 23:53:33 -0300

    Uso paleta.rkt. Imagen de paleta como pista. Mas gusanos, mejor ubicados y defasados.

--- nitelite.rkt
+++ nitelite.rkt
@@ -1,10 +1,19 @@
+; nuevo:
+; - usar paleta de colores
+; (antes ejecutar paleta.rkt)
+; - mas guachxs, mejor ubicados
+; - descoordinacion
+
+
 (clear)
 (show-fps 1)
 (desired-fps 1000000)
 ;(hint-wire)
-(scale (vmul vec1 1))
-(define (render n) (cond ((> n 0)
+(scale (vmul vec1 0.1))
+(define (render n m) (cond ((> n 0)
   ;(colour (vector (gh 0) (gh 1) (gh 2)))
+  ;(colour (list-ref paleta (random 6)))
+  (colour (list-ref paleta (modulo (+ n m) 6)))
   ;(scale (rsndtouch vec1 0.2))
   (push)
   ;(translate (rsndtouch vec1 0.1))
@@ -12,28 +21,33 @@
   (draw-sphere)
   (pop)
   ;(rotate (vector 0 (* (sin (time)) 100) (* 50 (cos (time)))))
-  (rotate (vector 0 (* (sintime 1) 100) (* 50 (costime 1))))
+  (rotate (vector 0 (* (sintime (* 0.1 m)) 100) (* 50 (costime (* 0.1 m)))))
   (translate (vector 0 1.3 0))
-  (render (- n 1)))))
+  (render (- n 1) m))))
 
 (define (render2 m v) (cond ((> m 0)
   (push)
-  (translate (vmul (car v) (* m 2)))
-  (render 5)
+    (rotate (rtouch (vector 90 0 0) 1))
+    (scale 50)
+    ;(colour p3)
+    (texture (load-texture "PALETA_COLAB_HEX.png"))
+    (draw-plane)
+  (pop)
+  (push)
+  (translate (vmul (car v) 20))
+  (render 6 m)
   (pop)
   (render2 (- m 1) (cdr v)))))
 
-;(every-frame (render2 10 (grndvecs2 10)))
-
-(let* ((x 10) (y (grndvecs2 x)))
+(let* ((x 20) (y (grndvecs2 x)))
     (every-frame (render2 x y)))
 
 
-(with-state
-    (rotate (vector 90 0 0))
-    (scale 50)
-    ;(colour (vector 255 0 0))
-    (build-plane))
+;(with-state
+;    (rotate (vector 90 0 0))
+;    (scale 50)
+;    (colour (vector 255 0 0))
+;    (build-plane))
 
 
 ;(light-diffuse 0 vec0)
