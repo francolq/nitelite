6eaf902024c0f2e655090648c414e6bf246c661c
Franco M. Luque <francolq@famaf.unc.edu.ar>
2020-12-15 23:56:52 -0300

    Movimiento de camara. Mejor codigo. Pruebo culitos pero no quedan bien.

--- nitelite.rkt
+++ nitelite.rkt
@@ -1,16 +1,22 @@
+; ANTES: cargar lib.rkt y paleta.rkt
+
 ; TODO:
+; - hacer obj invisible
+; - sacar culitos? (parecen pijas)
 ; - usar musica
-; - mover camara?
-; - poner culitos
-; - mejorar baile
+; - mejorar baile (saltitos)
+; - arreglar tabulacion
 
 ; nuevo:
+; - mejor codigo
+; - mover camara
+; - poner culitos
 ; - usar BPMs
 ; - luces locas
 ; - usar paleta de colores
-; (antes ejecutar paleta.rkt)
 ; - mas guachxs, mejor ubicados
 ; - descoordinacion
+
 
 (define bpms 127)
 
@@ -19,7 +25,9 @@
 (desired-fps 1000000)
 ;(hint-wire)
 (scale (vmul vec1 0.1))
-(define (render n m) (cond ((> n 0)
+
+
+(define (gusanito n m) (cond ((> n 0)
   ;(colour (vector (gh 0) (gh 1) (gh 2)))
   ;(colour (list-ref paleta (random 6)))
   (colour (list-ref paleta (modulo (+ n m) 6)))
@@ -27,14 +35,54 @@
   (push)
   ;(translate (rsndtouch vec1 0.1))
   (hint-cast-shadow)
+  ;(cond ((= n 5)
+  ;    (push)
+  ;    (translate (vector 0.5 0 0))
+  ;    (draw-sphere)
+  ;    (translate (vector -1 0 0))
+  ;    (draw-sphere)
+  ;    (pop))
+  ;      (else
+  ;    (draw-sphere)))
   (draw-sphere)
   (pop)
   ;(rotate (vector 0 (* (sin (time)) 100) (* 50 (cos (time)))))
   (rotate (vector 0 (* (sintime (* 0.1 m)) 100) (* 50 (costime (* 0.1 m)))))
   (translate (vector 0 1.3 0))
-  (render (- n 1) m))))
+  (gusanito (- n 1) m))))
 
-(define (render2 m v) (cond ((> m 0)
+
+(define (gusanitos m v) (cond ((> m 0)
+  (push)
+  (translate (vmul (car v) 15))
+  (gusanito 6 m)
+  (pop)
+  (gusanitos (- m 1) (cdr v)))))
+
+
+; LUCES
+; apagar luz principal
+(light-diffuse 0 vec0)
+(light-diffuse 0 (vec 0.2))
+(define key (make-light 'spot 'free))
+(light-position key (vector 5 5 0))
+(light-diffuse key (vector 1 0.95 0.8))
+(shadow-light key)
+
+
+; CAMARA
+(define obj (build-cube))
+(with-primitive obj
+    (scale (vec 11.5))
+    (translate (vector 0 -1 0)))
+(lock-camera obj)
+(camera-lag 0.1)
+; camera angle
+;(clip 1 10000)  ; dinamico, ver abajo
+
+
+(define (animate x y)
+  ; pista de baile
   ;(light-diffuse key (vector (sintime 1) (costime 1) 0.8))
   (light-diffuse key (vec (beatsin bpms)))
   (push)
@@ -44,22 +92,18 @@
     (texture (load-texture "PALETA_COLAB_HEX.png"))
     (draw-plane)
   (pop)
-  (push)
-  (translate (vmul (car v) 15))
-  (render 6 m)
-  (pop)
-  (render2 (- m 1) (cdr v)))))
+    (gusanitos x y)
+    ; camara
+    (with-primitive obj
+        ;(identity)
+        (rotate (vector 0.1 1 0))
+        ; 10 segundos, maxima cercania 4
+        ;(clip (min (fmod (time) 10) 4) 10000)
+        (clip (+ (sin (time)) 2) 10000)
+        ;(translate (vector 10 31 10))
+        ;(rotate (vector (sintime 10) 0 (costime 10)))
+        ;(translate (vector (fmod (* (time) 10) 10) 3 0))
+))
 
 (let* ((x 20) (y (grndvecs2 x)))
-    (every-frame (render2 x y)))
-
-; apagar luz principal
-;(light-diffuse 0 vec0)
-(light-diffuse 0 (vec 0.2))
-
-(define key (make-light 'spot 'free))
-(light-position key (vector 5 5 0))
-(light-diffuse key (vector 1 0.95 0.8))
-
-
-(shadow-light key)
+    (every-frame (animate x y)))
